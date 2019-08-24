c595e73398228c3c96dfbcf12eafc3d5980d05f4
Thomas Castagna <castagna.tm@gmail.com>
2019-08-24 18:08:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,10 @@ def load_library(file_path)
   file = YAML.load_file(file_path)
   result = {"get_meaning" => {}, "get_emoticon" => {} }
   file.each do |word, emoticons|
-    
-
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+    result["get_meaning"] [emoticons[1]] = emoticons[0]
+  end
+  result
 end
 
 def get_japanese_emoticon
