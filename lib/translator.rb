d00006d8ce7be4ce9f32eb4a193d1f3a8d917f75
Thomas Castagna <castagna.tm@gmail.com>
2019-08-24 18:18:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,9 @@ def load_library(file_path)
   result
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  file = YAML.load_file(file_path)
+
 end
 
 def get_english_meaning
